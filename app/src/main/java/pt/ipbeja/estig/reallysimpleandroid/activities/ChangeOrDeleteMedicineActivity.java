69c3a0b109b7a248333867227217e949a259ede4
JFEscola <jf.escola@outlook.pt>
2021-02-10 13:31:32 +0000

    Interface Updates

--- app/src/main/java/pt/ipbeja/estig/reallysimpleandroid/activities/ChangeOrDeleteMedicineActivity.java
+++ app/src/main/java/pt/ipbeja/estig/reallysimpleandroid/activities/ChangeOrDeleteMedicineActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,11 +18,13 @@ import pt.ipbeja.estig.reallysimpleandroid.db.entity.Medicine;
 
 public class ChangeOrDeleteMedicineActivity extends AppCompatActivity {
 
-    private HomeWatcher homeWatcher  = new HomeWatcher(this);
+    private final HomeWatcher homeWatcher  = new HomeWatcher(this);
     private EditText medicine;
     private EditText time;
     private Button update;
     private Button delete;
+    private Intent intent;
+    private CheckBox monday, tuesday, wednesday, thursday, friday, saturday, sunday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +35,21 @@ public class ChangeOrDeleteMedicineActivity extends AppCompatActivity {
         this.time = findViewById(R.id.timer_show_id);
         this.delete = findViewById(R.id.delete_medicine_id);
         this.update = findViewById(R.id.update_medicine_id);
-        Intent intent = getIntent();
+        this.monday = findViewById(R.id.monday_checkBox_change_id);
+        this.tuesday = findViewById(R.id.tuesday_checkBox_change_id);
+        this.wednesday = findViewById(R.id.wednesday_checkBox_change_id);
+        this.thursday = findViewById(R.id.thursday_checkBox_change_id);
+        this.friday = findViewById(R.id.friday_checkBox_change_id);
+        this.saturday = findViewById(R.id.saturday_checkBox_change_id);
+        this.sunday = findViewById(R.id.sunday_checkBox_change_id);
+        this.intent = getIntent();
 
         this.medicine.setText(intent.getStringExtra("medicineName"));
         this.time.setText(intent.getStringExtra("time"));
 
         Medicine medicineToChange = (Medicine) intent.getSerializableExtra("medicine");
+
+        setDaysBox();
 
         delete.setOnClickListener(v -> {
             Database.getINSTANCE(getApplicationContext()).medicineDao().delete(medicineToChange);
@@ -46,6 +58,21 @@ public class ChangeOrDeleteMedicineActivity extends AppCompatActivity {
         });
 
         update.setOnClickListener(v -> {
+
+            medicineToChange.setMonday(this.monday.isChecked());
+
+            medicineToChange.setTuesday(this.tuesday.isChecked());
+
+            medicineToChange.setWednesday(this.wednesday.isChecked());
+
+            medicineToChange.setThursday(this.thursday.isChecked());
+
+            medicineToChange.setFriday(this.friday.isChecked());
+
+            medicineToChange.setSaturday(this.saturday.isChecked());
+
+            medicineToChange.setSunday(this.sunday.isChecked());
+
             medicineToChange.setName(medicine.getText().toString());
             medicineToChange.setTime(time.getText().toString());
             Database.getINSTANCE(getApplicationContext()).medicineDao().update(medicineToChange);
@@ -56,6 +83,36 @@ public class ChangeOrDeleteMedicineActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         startHomeWatcher();
     }
+
+    private void setDaysBox(){
+        Medicine medicine = (Medicine) intent.getSerializableExtra("medicine");
+
+
+        if(medicine.isMonday()){
+            this.monday.setChecked(true);
+        }
+        if(medicine.isTuesday()){
+            this.tuesday.setChecked(true);
+        }
+        if(medicine.isWednesday()){
+            this.wednesday.setChecked(true);
+        }
+        if(medicine.isThursday()){
+            this.thursday.setChecked(true);
+        }
+        if(medicine.isFriday()){
+            this.friday.setChecked(true);
+        }
+        if(medicine.isSaturday()){
+            this.saturday.setChecked(true);
+        }
+        if(medicine.isSunday()){
+            this.sunday.setChecked(true);
+        }
+
+    }
+
+
 
     public void onHomeClicked(View view)
     {
